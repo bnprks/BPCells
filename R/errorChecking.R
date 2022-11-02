44079c655971182ff5da4566cd891d91c5acfa8f
Ben Parks <bnprks+git@gmail.com>
2022-11-02 10:33:23 -0700

    Format errorChecking

--- R/errorChecking.R
+++ R/errorChecking.R
@@ -4,8 +4,17 @@
 #   normalize_* functions return a normalized verison of the first argument, while
 #   raising an error via assert if the argument cannot be normalized.
 #   To avoid having the assert functions themselves in the call stack, all of
-#   them take an argument "n", which specifies how many calls on the stack to 
+#   them take an argument "n", which specifies how many calls on the stack to
 #   skip over during error printing.
+
+argument_name <- function(arg, n) {
+    arg_name <- substitute(arg)
+    for (f in seq_len(n)) {
+        arg_name <- do.call(substitute, list(arg_name, parent.frame(f)))
+    }
+    arg_name <- deparse(arg_name)
+    return(arg_name)
+}
 
 # Pretty print an error on behalf of a calling function
 # Goes up n levels on the call stack, and prints the offending
@@ -17,41 +26,38 @@
 # @param n Number of call levels to go up
 # @keywords internal
 pretty_error <- function(arg, msg, n) {
-    arg_name <- substitute(arg)
-    for (f in seq_len(n)) {
-        arg_name <- do.call(substitute, list(arg_name, parent.frame(f)))
-    }
-    arg_name <- deparse(arg_name)
+    arg_name <- argument_name(arg, n + 1)
 
-    fn_name <- rlang::call_name(sys.call(-(1+n)))
+    fn_name <- rlang::call_name(sys.call(-(1 + n)))
 
-    trace <- paste0(format(rlang::trace_back(bottom=parent.frame(n))), collapse="\n")
+    trace <- paste0(format(rlang::trace_back(bottom = parent.frame(n))), collapse = "\n")
 
     message <- sprintf(
         "In function %s: \"%s\" %s\n%s",
         fn_name, arg_name, msg, trace
     )
 
-    stop(message, call.=FALSE)
+    stop(message, call. = FALSE)
 }
 
-assert_wholenumber <- function(x, n=1) {
-    assert_is_numeric(x, n+1)
+assert_wholenumber <- function(x, n = 1) {
+    assert_is_numeric(x, n + 1)
     # Taken from documentation for is.integer
-    if(any(abs(x-round(x)) >= .Machine$double.eps^0.5))
-        pretty_error(x, "must be a whole number", n)
+    if (any(abs(x - round(x)) >= .Machine$double.eps^0.5)) {
+          pretty_error(x, "must be a whole number", n)
+      }
 }
 
-assert_is_numeric <- function(x, n=1) {
-    if(!is.numeric(x)) pretty_error(x, "must be numeric", n)
+assert_is_numeric <- function(x, n = 1) {
+    if (!is.numeric(x)) pretty_error(x, "must be numeric", n)
 }
 
-assert_is_character <- function(x, n=1) {
-    if(!is.character(x)) pretty_error(x, "must be a character vector", n)
+assert_is_character <- function(x, n = 1) {
+    if (!is.character(x)) pretty_error(x, "must be a character vector", n)
 }
 
-assert_len <- function(x, len, n=1) {
-    if(length(x) != len) pretty_error(x, sprintf("must have length %s", len), n)
+assert_len <- function(x, len, n = 1) {
+    if (length(x) != len) pretty_error(x, sprintf("must have length %s", len), n)
 }
 
 # Assertions for file paths, with adjustable criteria.
@@ -62,16 +68,16 @@ assert_len <- function(x, len, n=1) {
 # @param extension If not null, list of valid extensions (including the ".", e.g. ".tsv")
 # @param n How many caller frames above to use as the environment for error printing
 # @keywords internal
-assert_is_file <- function(path, must_exist=TRUE, multiple_ok=FALSE, extension=NULL, n=1) {
-    assert_is_character(path, n+1)
-    if (!multiple_ok) assert_len(path, 1, n+1)
+assert_is_file <- function(path, must_exist = TRUE, multiple_ok = FALSE, extension = NULL, n = 1) {
+    assert_is_character(path, n + 1)
+    if (!multiple_ok) assert_len(path, 1, n + 1)
 
     if (!is.null(extension)) {
         for (p in path) {
-            if(!any(endsWith(p, extension))) {
+            if (!any(endsWith(p, extension))) {
                 pretty_error(
-                    path, 
-                    sprintf("must have file extension %s", paste0(extension, collapse=", or ")),
+                    path,
+                    sprintf("must have file extension %s", paste0(extension, collapse = ", or ")),
                     n
                 )
             }
@@ -81,75 +87,78 @@ assert_is_file <- function(path, must_exist=TRUE, multiple_ok=FALSE, extension=N
     if (must_exist && !all(file.exists(path))) pretty_error(path, "is not a valid file path (file not found)", n)
 }
 
-assert_distinct <- function(vector, n=1) {
-    if(anyDuplicated(vector)) pretty_error(vector, "must not have duplicated values", n)
+assert_distinct <- function(vector, n = 1) {
+    if (anyDuplicated(vector)) pretty_error(vector, "must not have duplicated values", n)
 }
-assert_greater_than_zero <- function(vector, n=1) {
-    assert_is_numeric(vector, n+1)
-    if(any(vector <= 0)) pretty_error(vector, "must be greater than zero", n)
+assert_greater_than_zero <- function(vector, n = 1) {
+    assert_is_numeric(vector, n + 1)
+    if (any(vector <= 0)) pretty_error(vector, "must be greater than zero", n)
 }
-assert_is <- function(object, class, n=1) {
+assert_is <- function(object, class, n = 1) {
     if (length(class) == 1) {
-        if(!is(object, class)) pretty_error(object, sprintf("must have class \"%s\"", class), n)
+        if (!is(object, class)) pretty_error(object, sprintf("must have class \"%s\"", class), n)
     } else {
         match <- FALSE
         for (c in class) {
             match <- is(object, c) || match
         }
-        if (!match) pretty_error(object, sprintf("must have class %s", paste0(class, collapse=", or ")), n)
+        if (!match) pretty_error(object, sprintf("must have class %s", paste0(class, collapse = ", or ")), n)
     }
 }
 
-assert_true <- function(expr, n=1) {
-    if(!expr) pretty_error(expr, "is not true", n)
+assert_true <- function(expr, n = 1) {
+    if (!expr) pretty_error(expr, "is not true", n)
 }
 
-assert_has_names <- function(x, names, n=1) {
-    if (!all(names %in% names(x))) 
-        pretty_error(x, sprintf("is missing names: %s", paste0(setdiff(names, names(x)), collapse=", ")), n)
+assert_has_names <- function(x, names, n = 1) {
+    if (!all(names %in% names(x))) {
+          pretty_error(x, sprintf("is missing names: %s", paste0(setdiff(names, names(x)), collapse = ", ")), n)
+      }
 }
 
-assert_not_na <- function(x, n=1) {
+assert_not_na <- function(x, n = 1) {
     if (any(is.na(x))) pretty_error(x, "has NA entries", n)
 }
 
-assert_not_null <- function(x, n=1) {
+assert_not_null <- function(x, n = 1) {
     if (is.null(x)) pretty_error(x, "is NULL", n)
 }
 
 
-normalize_length <- function(x, len, n=1) {
-    if (length(x) == 1)
-        return(rep(x, len))
-    assert_len(x, len, n+1)
+normalize_length <- function(x, len, n = 1) {
+    if (length(x) == 1) {
+          return(rep(x, len))
+      }
+    assert_len(x, len, n + 1)
     return(x)
 }
 
-assert_has_package <- function(packages, n=1) {
+assert_has_package <- function(packages, n = 1) {
     missing <- c()
     for (p in packages) {
-        if (!requireNamespace(p, quietly = TRUE))
-            missing <- c(missing, p)
+        if (!requireNamespace(p, quietly = TRUE)) {
+              missing <- c(missing, p)
+          }
     }
     if (length(missing) > 0) {
-        stop(sprintf("Missing required package(s): %s", paste0(missing, collapse=", ")), .call=FALSE)
+        stop(sprintf("Missing required package(s): %s", paste0(missing, collapse = ", ")), .call = FALSE)
     }
 }
 
 
 #' Normalize an object representing genomic ranges
-#' 
+#'
 #' @param ranges List, GRanges, or data.frame object. Must have start, end, and chr defined
 #' @param metadata_cols Optional list of metadata columns to require & extract
 #' @param zero_based_coords If true, coordinates start and 0 and the end coordinate is not included in the range.
 #'  If false, coordinates start at 1 and the end coordinate is included in the range
 #' @return list object with zero-based coordinates, and elements chr (factor), start (int), and end (int).
 #' If `ranges` does not have chr level information, chr levels are the sorted unique values of chr.
-#' 
+#'
 #' If strand is in metadata_cols, then the output strand element will be TRUE for positive strand,
 #' and FALSE for negative strand. (Converted from a character vector of "+"/"-" if necessary)
-normalize_ranges <- function(ranges, metadata_cols=character(0), zero_based_coords=!is(ranges,"GRanges")) {
-    assert_is(ranges, c("GRanges", "list", "data.frame"), n=2)
+normalize_ranges <- function(ranges, metadata_cols = character(0), zero_based_coords = !is(ranges, "GRanges")) {
+    assert_is(ranges, c("GRanges", "list", "data.frame"), n = 2)
     assert_is(metadata_cols, "character")
     assert_is(zero_based_coords, "logical")
 
@@ -157,10 +166,10 @@ normalize_ranges <- function(ranges, metadata_cols=character(0), zero_based_coor
     if (is(ranges, "GRanges")) {
         if ("strand" %in% metadata_cols) ret$strand <- as.logical(GenomicRanges::strand(ranges) != "-")
         metadata_cols <- metadata_cols[metadata_cols != "strand"]
-        
-        assert_has_names(GenomicRanges::mcols(ranges), metadata_cols, n=2)
+
+        assert_has_names(GenomicRanges::mcols(ranges), metadata_cols, n = 2)
         chr_levels <- GenomeInfoDb::seqlevels(ranges)
-        ret$chr <- factor(as.character(GenomicRanges::seqnames(ranges)), levels=chr_levels)
+        ret$chr <- factor(as.character(GenomicRanges::seqnames(ranges)), levels = chr_levels)
         ret$start <- GenomicRanges::start(ranges) - !zero_based_coords
         ret$end <- GenomicRanges::end(ranges)
 
@@ -168,16 +177,19 @@ normalize_ranges <- function(ranges, metadata_cols=character(0), zero_based_coor
             ret[[c]] <- GenomicRanges::mcols(ranges)[[c]]
         }
     } else {
-        assert_has_names(ranges, c("chr", "start", "end", metadata_cols), n=2)
+        assert_has_names(ranges, c("chr", "start", "end", metadata_cols), n = 2)
         chr_levels <- levels(ranges$chr)
         if (is.null(chr_levels)) chr_levels <- sort(unique(as.character(ranges$chr)))
-        ret$chr <- factor(as.character(ranges$chr), levels=chr_levels)
+        ret$chr <- factor(as.character(ranges$chr), levels = chr_levels)
         ret$start <- as.integer(ranges$start) - !zero_based_coords
         ret$end <- as.integer(ranges$end)
         if ("strand" %in% metadata_cols) {
             assert_is(ranges$strand, c("character", "logical"))
-            if (is(ranges$strand, "character")) ret$strand <- ranges$strand != "-"
-            else ret$strand <- ranges$strand # If strand is already logical
+            if (is(ranges$strand, "character")) {
+                ret$strand <- ranges$strand != "-"
+            } else {
+                ret$strand <- ranges$strand
+            } # If strand is already logical
         }
         metadata_cols <- metadata_cols[metadata_cols != "strand"]
         for (c in metadata_cols) {
