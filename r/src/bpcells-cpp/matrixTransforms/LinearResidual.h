d4a8ac35a0354a15924d1725b58d41a73a61d111
Ben Parks <bnprks+git@gmail.com>
2024-07-15 21:11:17 -0700

    [cpp] Update LinearResidual copyright year

--- r/src/bpcells-cpp/matrixTransforms/LinearResidual.h
+++ r/src/bpcells-cpp/matrixTransforms/LinearResidual.h
@@ -1,4 +1,4 @@
-// Copyright 2022 BPCells contributors
+// Copyright 2024 BPCells contributors
 // 
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
