129cd673bd1fdf8d0d74122cd8a1402604b24c3f
immanuelazn <immanuelazn@gmail.com>
2024-09-05 21:35:16 -0700

    [r] add pseubulk test case

--- r/tests/testthat/test-pseudobulk.R
+++ r/tests/testthat/test-pseudobulk.R
@@ -8,17 +8,19 @@ generate_dense_matrix <- function(nrow, ncol) {
   matrix(runif(nrow * ncol), nrow = nrow)
 }
 
-# test_that("Pseudobulk aggregation works", {
-#   # Test with mean and sum
-# 
-# )
-m0 <- generate_sparse_matrix(20, 10, max_val = 10)
-m1 <- m0 |> as("IterableMatrix")
-groups <- c(rep.int(1, 5), rep.int(2, 5))
-m0 <- as.matrix(m0)
-#m0 <- tibble::as_tibble(m0)
-m0_sum <- suppressWarnings(m0 %>% t() %>% tibble::as_tibble() %>% dplyr::mutate(group = groups) %>% dplyr::group_by(group) %>% dplyr::summarise_each(dplyr::funs(sum)) %>% t())
-colnames(m0_sum) <- m0_sum[1,]
-m0_sum <- m0_sum[-1,]
-browser()
-m1_sum <- pseudobulk_counts(m1, groups, approach = "sum")}
+test_that("Pseudobulk aggregation works", {
+  # Test with mean and sum
+  m0 <- generate_sparse_matrix(20, 10, max_val = 10)
+  m1 <- m0 |> as("IterableMatrix")
+  groups <- c(rep.int(1, 5), rep.int(2, 5))
+  m0 <- as.matrix(m0)
+  #m0 <- tibble::as_tibble(m0)
+  m0_sum <- suppressWarnings(m0 %>% t() %>% tibble::as_tibble() %>% dplyr::mutate(group = groups) %>% dplyr::group_by(group) %>% dplyr::summarise_each(dplyr::funs(sum)) %>% t())
+  m0_mean <- suppressWarnings(m0 %>% t() %>% tibble::as_tibble() %>% dplyr::mutate(group = groups) %>% dplyr::group_by(group) %>% dplyr::summarise_each(dplyr::funs(mean)) %>% t())
+  colnames(m0_sum) <- m0_sum[1,]
+  m0_sum <- m0_sum[-1,]
+  m1_sum <- pseudobulk_counts(m1, groups, approach = "sum")
+  m1_mean <- pseudobulk_counts(m1, groups, approach = "mean")
+  expect_equal(m0_sum, m1_sum)
+  expect_equal(m0_mean, m1_mean)
+})
