7628d0183ad21bc89c1961b320624b07ecbea288
Immanuel Abdi <immanuelazn@gmail.com>
2024-10-25 01:36:25 -0700

    [r]  add automatic website building (#39)

---
+++ .github/workflows/deploy-reference.R
@@ -0,0 +1,21 @@
+build_selective_vignette_changes <- function() {
+    vignettes_folder <- "r/vignettes/"
+    # Get all changes in the last commit
+    changed_files <- system("git diff --name-only HEAD~1", intern = TRUE)
+    # split to only get files in r/vignettes
+    changed_vignettes <- changed_files[grepl(vignettes_folder, changed_files)]
+    for (vgn in changed_vignettes) {
+      pkgdown::build_article(gsub("\\.Rmd$", "", gsub("r/vignettes/", "", vgn)), 'r')
+    }
+}
+args = commandArgs(TRUE)
+# First argument is whether to rebuild the entire site.
+rebuild <- as.logical(args[1])
+if (rebuild) {
+  pkgdown::build_site_github_pages('r')
+} else {
+  pkgdown::build_reference('r')
+  build_selective_vignette_changes()
+  pkgdown::build_news('r')
+  pkgdown::build_home('r')
+}
