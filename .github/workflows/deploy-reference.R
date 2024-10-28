c9000753136733d28a048591ba2086755536ed6a
immanuelazn <immanuelazn@gmail.com>
2024-10-28 13:36:27 -0700

    [cicd] add suggested changes for pkgdown building

--- .github/workflows/deploy-reference.R
+++ .github/workflows/deploy-reference.R
@@ -2,8 +2,9 @@ build_selective_vignette_changes <- function() {
     vignettes_folder <- "r/vignettes/"
     # Get all changes in the last commit
     changed_files <- system("git diff --name-only HEAD~1", intern = TRUE)
-    # split to only get files in r/vignettes
+    # split to only get files in r/vignettes and only get the Rmd files
     changed_vignettes <- changed_files[grepl(vignettes_folder, changed_files)]
+    changed_vignettes <- changed_vignettes[grepl("\\.Rmd$", changed_vignettes)]
     for (vgn in changed_vignettes) {
       pkgdown::build_article(gsub("\\.Rmd$", "", gsub("r/vignettes/", "", vgn)), 'r')
     }
@@ -17,5 +18,8 @@ if (rebuild) {
   pkgdown::build_reference('r')
   build_selective_vignette_changes()
   pkgdown::build_news('r')
+  pkgdown::build_sitemap('r')
+  pkgdown::build_redirects('r')
+  pkgdown::build_search('r')
   pkgdown::build_home('r')
 }
