dba9f5718bde5cbdd77a938c75a9103c2e6993e6
immanuelazn <immanuelazn@gmail.com>
2024-10-28 15:08:35 -0700

    [cicd] add in build_article_index()

--- .github/workflows/deploy-reference.R
+++ .github/workflows/deploy-reference.R
@@ -11,6 +11,7 @@ build_selective_vignette_changes <- function() {
     vignettes_folder <- "r/vignettes/"
     # Get all changes in the last commit
     changed_files <- system("git diff --name-only HEAD~1", intern = TRUE)
+    pkgdown::build_articles_index('r')
     # split to only get files in r/vignettes and only get the Rmd files
     changed_vignettes <- changed_files[grepl(vignettes_folder, changed_files)]
     changed_vignettes <- changed_vignettes[grepl("\\.Rmd$", changed_vignettes)]
