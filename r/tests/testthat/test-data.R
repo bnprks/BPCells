a24160481ab60871650a47010013c6ef151b95da
immanuelazn <immanuelazn@gmail.com>
2025-04-10 12:06:13 -0700

    [r] skip test for preparing demo data

--- r/tests/testthat/test-data.R
+++ r/tests/testthat/test-data.R
@@ -8,10 +8,11 @@
 
 
 test_that("Getting test data works", {
-    expect_no_error(BPCells:::prepare_demo_data(file.path(tools::R_user_dir("BPCells", which = "data"), "demo_data")))
     mat <- get_demo_mat()
     frags <- get_demo_frags()
     expect_true(is(mat, "IterableMatrix"))
     expect_true(is(frags, "IterableFragments"))
     remove_demo_data()
+    skip("Skip preparing demo data")
+    expect_no_error(BPCells:::prepare_demo_data(file.path(tools::R_user_dir("BPCells", which = "data"), "demo_data")))
 })
