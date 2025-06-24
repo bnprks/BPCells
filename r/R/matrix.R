13c1cbe43fe1e731e4f91487a6d91ffc72e9cafe
Immanuel Abdi <56730419+immanuelazn@users.noreply.github.com>
2025-06-24 10:58:20 -0700

    [r] Function examples for matrix ops (#207)

--- r/R/matrix.R
+++ r/R/matrix.R
@@ -1588,6 +1588,19 @@ setMethod("iterate_matrix", "UnpackedMatrixMem_double", function(x) {
 #' passes through the data, and ~7.3TB of data to be sorted in three passes
 #' through the data.
 #' @return MatrixDir object with a copy of the input matrix, but the storage order flipped
+#' @examples
+#' mat <- matrix(rnorm(50), nrow = 10, ncol = 5)
+#' rownames(mat) <- paste0("gene", seq_len(10))
+#' colnames(mat) <- paste0("cell", seq_len(5))
+#' mat <- mat %>% as("dgCMatrix") %>% as("IterableMatrix")
+#' mat
+#' 
+#' ## A regular transpose operation switches a user's rows and cols 
+#' t(mat)
+#' 
+#' ## Running `transpose_storage_order()` instead changes whether the storage is in row-major or col-major,
+#' ## but does not switch the rows and cols
+#' transpose_storage_order(mat)
 #' @export
 transpose_storage_order <- function(matrix, outdir = tempfile("transpose"), tmpdir = tempdir(), load_bytes = 4194304L, sort_bytes = 1073741824L) {
   assert_true(matrix_type(matrix) %in% c("uint32_t", "float", "double"))
@@ -2818,6 +2831,13 @@ setMethod("[", "ConvertMatrixType", function(x, i, j, ...) {
 #' @param type One of uint32_t (unsigned 32-bit integer), float (32-bit real number),
 #'   or double (64-bit real number)
 #' @return IterableMatrix object
+#' @examples
+#' mat <- matrix(rnorm(50), nrow = 10, ncol = 5)
+#' rownames(mat) <- paste0("gene", seq_len(10))
+#' colnames(mat) <- paste0("cell", seq_len(5))
+#' mat <- mat %>% as("dgCMatrix") %>% as("IterableMatrix")
+#' mat
+#' convert_matrix_type(mat, "float")
 #' @export
 convert_matrix_type <- function(matrix, type = c("uint32_t", "double", "float")) {
   assert_is(matrix, c("dgCMatrix", "IterableMatrix"))
@@ -2992,6 +3012,27 @@ setMethod("as.matrix", signature(x = "IterableMatrix"), function(x, ...) as(x, "
 #' less complex stats are calculated in the process of calculating a more complicated stat.
 #' So to calculate mean and variance simultaneously, just ask for variance,
 #' which will compute mean and nonzero counts as a side-effect
+#' @examples
+#' mat <- matrix(rpois(100, lambda = 5), nrow = 10)
+#' rownames(mat) <- paste0("gene", 1:10)
+#' colnames(mat) <- paste0("cell", 1:10)
+#' mat <- mat %>% as("dgCMatrix") %>% as("IterableMatrix")
+#' 
+#' ## By default, no row or column stats are calculated
+#' res_none <- matrix_stats(mat)
+#' res_none
+#' 
+#' ## Request row variance (automatically computes mean and nonzero too)
+#' res_row_var <- matrix_stats(mat, row_stats = "variance")
+#' res_row_var
+#' 
+#' ## Request both row variance and column variance
+#' res_both_var <- matrix_stats(
+#'   mat = mat,
+#'   row_stats = "variance",
+#'   col_stats = "mean"
+#' )
+#' res_both_var
 #' @export
 matrix_stats <- function(matrix,
                          row_stats = c("none", "nonzero", "mean", "variance"),
@@ -3152,6 +3193,34 @@ checksum <- function(matrix) {
 #' @seealso For an interface more similar to `base::apply`, see the [BPCellsArray](https://github.com/Yunuuuu/BPCellsArray/)
 #' project. For calculating colMeans on a sparse single cell RNA matrix it is about 8x slower than `apply_by_col`, due to the
 #' `base::apply` interface not being sparsity-aware. (See [pull request #104](https://github.com/bnprks/BPCells/pull/104) for benchmarking.)
+#' @examples
+#' mat <- matrix(rbinom(40, 1, 0.5) * sample.int(5, 40, replace = TRUE), nrow = 4)
+#' rownames(mat) <- paste0("gene", 1:4)
+#' mat
+#' 
+#' mat <- mat %>% as("dgCMatrix") %>% as("IterableMatrix")
+#' 
+#' #######################################################################
+#' ## apply_by_row() example
+#' #######################################################################
+#' ## Get mean of every row
+#' 
+#' ## expect an error in the case that col-major matrix is passed
+#' apply_by_row(mat, function(val, row, col) {sum(val) / nrow(mat)}) %>% 
+#'  unlist()
+#' 
+#' ## Need to transpose matrix to make sure it is in row-order
+#' mat_row_order <- transpose_storage_order(mat)
+#' 
+#' ## works as expected for row major
+#' apply_by_row(mat_row_order, 
+#'  function(val, row, col) sum(val) / ncol(mat_row_order)
+#' ) %>% unlist()
+#' 
+#' # Also analogous to running rowMeans() without names
+#' rowMeans(mat)
+#' 
+#' 
 #' @export
 apply_by_row <- function(mat, fun, ...) {
   assert_is(mat, "IterableMatrix")
@@ -3168,6 +3237,16 @@ apply_by_row <- function(mat, fun, ...) {
 
 #' @return **apply_by_col** - A list of length `ncol(matrix)` with the results returned by `fun()` on each row
 #' @rdname apply_by_row
+#' @examples
+#' #######################################################################
+#' ## apply_by_col() example
+#' #######################################################################
+#' ## Get argmax of every col
+#' apply_by_col(mat, 
+#'  function(val, row, col) if (length(val) > 0) row[which.max(val)] else 1L
+#' ) %>% unlist()
+#' 
+#' 
 #' @export
 apply_by_col <- function(mat, fun, ...) {
   if (storage_order(mat) != "col") {
--- r/R/transforms.R
+++ r/R/transforms.R
@@ -181,6 +181,21 @@ setMethod("short_description", "TransformMin", function(x) {
 #' @return IterableMatrix
 #' @description **min_scalar**: Take minumum with a global constant
 #' @rdname min_elementwise
+#' @examples
+#' set.seed(12345)
+#' mat <- matrix(rpois(40, lambda = 5), nrow = 4)
+#' rownames(mat) <- paste0("gene", 1:4)
+#' 
+#' mat <- mat %>% as("dgCMatrix")
+#' mat
+#' mat <- mat %>% as("IterableMatrix")
+#' 
+#' #######################################################################
+#' ## min_scalar() example
+#' #######################################################################
+#' min_scalar(mat, 4) %>% as("dgCMatrix")
+#' 
+#' 
 #' @export
 min_scalar <- function(mat, val) {
   assert_is(mat, "IterableMatrix")
@@ -214,6 +229,13 @@ setMethod("short_description", "TransformMinByRow", function(x) {
 
 #' @rdname min_elementwise
 #' @description **min_by_row**: Take the minimum with a per-row constant
+#' @examples
+#' #######################################################################
+#' ## min_by_row() example
+#' #######################################################################
+#' min_by_row(mat, 1:4) %>% as("dgCMatrix")
+#' 
+#' 
 #' @export
 min_by_row <- function(mat, vals) {
   if (mat@transpose) {
@@ -247,6 +269,13 @@ setMethod("short_description", "TransformMinByCol", function(x) {
 
 #' @rdname min_elementwise
 #' @description **min_by_col**: Take the minimum with a per-col constant
+#' @examples
+#' #######################################################################
+#' ## min_by_col() example
+#' #######################################################################
+#' min_by_col(mat, 1:10) %>% as("dgCMatrix")
+#' 
+#' 
 #' @export
 min_by_col <- function(mat, vals) {
   if (mat@transpose) {
@@ -781,6 +810,21 @@ setMethod("+", signature(e1 = "numeric", e2 = "TransformScaleShift"), function(e
 #' @param mat Matrix-like object
 #' @param vec Numeric vector
 #' @return Matrix-like object
+#' @examples
+#' set.seed(12345)
+#' mat <- matrix(rpois(40, lambda = 5), nrow = 4)
+#' rownames(mat) <- paste0("gene", 1:4)
+#' 
+#' mat <- mat %>% as("dgCMatrix")
+#' mat
+#' mat <- mat %>% as("IterableMatrix")
+#' 
+#' #######################################################################
+#' ## add_rows() example
+#' #######################################################################
+#' add_rows(mat, 1:4) %>% as("dgCMatrix")
+#' 
+#' 
 #' @export
 add_rows <- function(mat, vec) {
   assert_is(mat, c("dgCMatrix", "IterableMatrix", "matrix"))
@@ -789,6 +833,13 @@ add_rows <- function(mat, vec) {
   mat + vec
 }
 #' @rdname mat_norm
+#' @examples
+#' #######################################################################
+#' ## add_cols() example
+#' #######################################################################
+#' add_cols(mat, 1:10) %>% as("dgCMatrix")
+#' 
+#' 
 #' @export
 add_cols <- function(mat, vec) {
   assert_is(mat, c("dgCMatrix", "IterableMatrix", "matrix"))
@@ -797,6 +848,13 @@ add_cols <- function(mat, vec) {
   t(t(mat) + vec)
 }
 #' @rdname mat_norm
+#' @examples
+#' #######################################################################
+#' ## multiply_rows() example
+#' #######################################################################
+#' multiply_rows(mat, 1:4) %>% as("dgCMatrix")
+#' 
+#' 
 #' @export
 multiply_rows <- function(mat, vec) {
   assert_is(mat, c("dgCMatrix", "IterableMatrix", "matrix"))
@@ -805,6 +863,13 @@ multiply_rows <- function(mat, vec) {
   mat * vec
 }
 #' @rdname mat_norm
+#' @examples
+#' #######################################################################
+#' ## multiply_cols() example
+#' #######################################################################
+#' multiply_cols(mat, 1:10) %>% as("dgCMatrix")
+#' 
+#' 
 #' @export
 multiply_cols <- function(mat, vec) {
   assert_is(mat, c("dgCMatrix", "IterableMatrix", "matrix"))
